9114163decfdd5901866cbc95971172e92aba842
pyrox18 <haryzizzudin@gmail.com>
2018-04-19 17:12:10 +0800

    Add bill validator method

--- bill.go
+++ bill.go
@@ -1,5 +1,12 @@
 package billplz
 
+import (
+	"regexp"
+
+	"github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+)
+
 type Bill struct {
 	ID              string `json:"id,omitempty"`
 	CollectionID    string `json:"collection_id,omitempty"`
@@ -20,4 +27,23 @@ type Bill struct {
 	RedirectURL     string `json:"redirect_url,omitempty"`
 	CallbackURL     string `json:"callback_url,omitempty"`
 	Description     string `json:"description,omitempty"`
+}
+
+func (b *Bill) Validate() error {
+	err := validation.Errors{
+		"collection_id":     validation.Validate(b.CollectionID, validation.Required),
+		"email":             validation.Validate(b.Email, is.Email),
+		"mobile":            validation.Validate(b.Mobile, validation.Match(regexp.MustCompile(`\+?60\d{8,10}`))),
+		"name":              validation.Validate(b.Name, validation.Required),
+		"amount":            validation.Validate(b.Amount, validation.Required),
+		"callback_url":      validation.Validate(b.CallbackURL, validation.Required, is.URL),
+		"description":       validation.Validate(b.Description, validation.Required, validation.Length(1, 200)),
+		"due_at":            validation.Validate(b.DueAt, validation.Date("2018-04-19")),
+		"redirect_url":      validation.Validate(b.RedirectURL, is.URL),
+		"reference_1_label": validation.Validate(b.Reference1Label, validation.Length(0, 20)),
+		"reference_1":       validation.Validate(b.Reference1, validation.Length(0, 120)),
+		"reference_2_label": validation.Validate(b.Reference2Label, validation.Length(0, 20)),
+		"reference_2":       validation.Validate(b.Reference2, validation.Length(0, 120)),
+	}.Filter()
+	return err
 }
