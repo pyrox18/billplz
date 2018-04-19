2729154827c3fe118c5eb19d60f4f8c9d9cf0822
pyrox18 <haryzizzudin@gmail.com>
2018-04-19 09:38:37 +0800

    Create bank account struct

---
+++ bank-account.go
@@ -0,0 +1,13 @@
+package billplz
+
+type BankAccount struct {
+	Name              string `json:"name,omitempty"`
+	IDNumber          string `json:"id_no,omitempty"`
+	AccountNumber     string `json:"acc_no,omitempty"`
+	Code              string `json:"code,omitempty"`
+	Organization      bool   `json:"organization,omitempty"`
+	AuthorizationDate string `json:"authorization_date,omitempty"`
+	Status            string `json:"status,omitempty"`
+	ProcessedAt       string `json:"processed_at,omitempty"`
+	RejectDescription string `json:"reject_desc,omitempty"`
+}
