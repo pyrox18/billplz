567a298c375fa1c9d453da50296f1277676633c7
pyrox18 <haryzizzudin@gmail.com>
2018-04-20 12:13:11 +0800

    Add docs for bank account

--- bank-account.go
+++ bank-account.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-ozzo/ozzo-validation"
 )
 
+// BankAccount represents a bank account stored in the Billplz API.
 type BankAccount struct {
 	Name              string `json:"name,omitempty"`
 	IDNumber          string `json:"id_no,omitempty"`
@@ -16,6 +17,7 @@ type BankAccount struct {
 	RejectDescription string `json:"reject_desc,omitempty"`
 }
 
+// Validate validates a bank account's details for submission with Client.CreateBankAccount.
 func (b *BankAccount) Validate() error {
 	err := validation.Errors{
 		"name":   validation.Validate(b.Name, validation.Required),
@@ -26,10 +28,13 @@ func (b *BankAccount) Validate() error {
 	return err
 }
 
+// BankAccountCheckResponse represents the structure of the response body obtained with
+// Client.CheckRegistration.
 type BankAccountCheckResponse struct {
 	Name string `json:"name,omitempty"`
 }
 
+// BankAccountList represents the structure of the response body obtained with Client.GetBankAccountIndex.
 type BankAccountList struct {
 	BankAccounts *[]BankAccount `json:"bank_verification_services,omitempty"`
 }
