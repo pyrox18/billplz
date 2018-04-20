013738c8a2bfe0ac693ab1d77796bc9f0b69757d
pyrox18 <haryzizzudin@gmail.com>
2018-04-20 15:27:10 +0800

    Add docs for transaction

--- transaction.go
+++ transaction.go
@@ -2,6 +2,7 @@ package billplz
 
 import "encoding/json"
 
+// Transaction represents a transaction made for a bill.
 type Transaction struct {
 	ID             string `json:"id,omitempty"`
 	Status         string `json:"status,omitempty"`
@@ -9,6 +10,7 @@ type Transaction struct {
 	PaymentChannel string `json:"payment_channel,omitempty"`
 }
 
+// BillTransactions represent the structure of a list of transactions received from the Billplz API.
 type BillTransactions struct {
 	BillID       string         `json:"bill_id,omitempty"`
 	Transactions *[]Transaction `json:"transactions,omitempty"`
