a417f3c9a00ffd1fe79bc5ad64a2e351f1a8b1f4
pyrox18 <haryzizzudin@gmail.com>
2018-04-23 15:59:19 +0800

    Add docs for client

--- client.go
+++ client.go
@@ -9,6 +9,8 @@ import (
 	"strconv"
 )
 
+// Client represents the HTTP client that interacts with the Billplz API. The
+// Client stores the API key used for authentication with the API.
 type Client struct {
 	baseURL    *url.URL
 	httpClient *http.Client
@@ -16,6 +18,11 @@ type Client struct {
 	APIKey string
 }
 
+// NewClient instantiates and returns a new Client.
+// If a http.Client is not supplied, the function will use the default HTTP client.
+// An API key must be provided for the client to authenticate with the API.
+// The sandbox boolean value determines whether the client will communicate with
+// the sandbox endpoint or the production endpoint.
 func NewClient(httpClient *http.Client, apiKey string, sandbox bool) (*Client, error) {
 	if httpClient == nil {
 		httpClient = http.DefaultClient
@@ -36,6 +43,9 @@ func NewClient(httpClient *http.Client, apiKey string, sandbox bool) (*Client, e
 	return c, err
 }
 
+// CreateCollection creates a new collection.
+// An error will be returned if the supplied collection fails validation,
+// or if the HTTP request fails.
 func (c *Client) CreateCollection(collection Collection) (*Collection, error) {
 	if collection.SplitPayment == nil {
 		collection.SplitPayment = &SplitPayment{}
@@ -55,6 +65,9 @@ func (c *Client) CreateCollection(collection Collection) (*Collection, error) {
 	return &result, err
 }
 
+// GetCollection retrieves a single collection with the given ID.
+// An error will be returned if the collection is not found, or if
+// the HTTP request fails.
 func (c *Client) GetCollection(id string) (*Collection, error) {
 	req, err := c.newRequest(http.MethodGet, "/collections/"+id, nil)
 	if err != nil {
@@ -69,6 +82,14 @@ func (c *Client) GetCollection(id string) (*Collection, error) {
 	return &result, err
 }
 
+// GetCollectionIndex retrieves a set of collections. Up to 15 collections
+// will be returned at a time.
+// The page parameter determines the page of the collection set to retrieve,
+// and defaults to 1.
+// The status parameter determines whether to retrieve all collections, or
+// only collections that are active or inactive. This parameter can take
+// values of "", "active" or "inactive", and will default to "".
+// An error will be returned if the HTTP request fails.
 func (c *Client) GetCollectionIndex(page int, status string) (*CollectionIndexResult, error) {
 	if page <= 0 {
 		page = 1
@@ -94,6 +115,9 @@ func (c *Client) GetCollectionIndex(page int, status string) (*CollectionIndexRe
 	return &result, err
 }
 
+// CreateOpenCollection creates a new open collection.
+// An error will be returned if the supplied open collection fails
+// validation, or if the HTTP request fails.
 func (c *Client) CreateOpenCollection(o OpenCollection) (*OpenCollection, error) {
 	if o.SplitPayment == nil {
 		o.SplitPayment = &SplitPayment{}
@@ -113,6 +137,9 @@ func (c *Client) CreateOpenCollection(o OpenCollection) (*OpenCollection, error)
 	return &result, err
 }
 
+// GetOpenCollection retrieves a single open collection with the given ID.
+// An error will be returned if the open collection is not found, or if
+// the HTTP request fails.
 func (c *Client) GetOpenCollection(id string) (*OpenCollection, error) {
 	req, err := c.newRequest(http.MethodGet, "/open_collections/"+id, nil)
 	if err != nil {
@@ -127,6 +154,14 @@ func (c *Client) GetOpenCollection(id string) (*OpenCollection, error) {
 	return &result, err
 }
 
+// GetOpenCollectionIndex retrieves a set of open collections. Up to 15
+// collections will be returned at a time.
+// The page parameter determines the page of the open collection set to retrieve,
+// and defaults to 1.
+// The status parameter determines whether to retrieve all open collections, or
+// only open collections that are active or inactive. This parameter can take
+// values of "", "active" or "inactive", and will default to "".
+// An error will be returned if the HTTP request fails.
 func (c *Client) GetOpenCollectionIndex(page int, status string) (*OpenCollectionIndexResult, error) {
 	if page == 0 {
 		page = 1
@@ -152,6 +187,9 @@ func (c *Client) GetOpenCollectionIndex(page int, status string) (*OpenCollectio
 	return &result, err
 }
 
+// DeactivateCollection deactivates a collection with the given ID.
+// An error will be returned if the collection could not be deactivated,
+// or if the HTTP request fails.
 func (c *Client) DeactivateCollection(id string) error {
 	req, err := c.newRequest(http.MethodPost, "/collections/"+id+"/deactivate", nil)
 	if err != nil {
@@ -165,6 +203,9 @@ func (c *Client) DeactivateCollection(id string) error {
 	return nil
 }
 
+// ActivateCollection activates a collection with the given ID.
+// An error will be returned if the collection could not be activated, or
+// if the HTTP request fails.
 func (c *Client) ActivateCollection(id string) error {
 	req, err := c.newRequest(http.MethodPost, "/collections/"+id+"/activate", nil)
 	if err != nil {
@@ -178,6 +219,9 @@ func (c *Client) ActivateCollection(id string) error {
 	return nil
 }
 
+// CreateBill creates a new bill.
+// An error will be returned if the supplied bill fails validation,
+// or if the HTTP request fails.
 func (c *Client) CreateBill(b Bill) (*Bill, error) {
 	err := b.Validate()
 	if err != nil {
@@ -194,6 +238,9 @@ func (c *Client) CreateBill(b Bill) (*Bill, error) {
 	return &result, err
 }
 
+// GetBill retrieves a single bill with the given ID.
+// An error will be returned if the bill is not found, or
+// if the HTTP request fails.
 func (c *Client) GetBill(id string) (*Bill, error) {
 	req, err := c.newRequest(http.MethodGet, "/bills/"+id, nil)
 	if err != nil {
@@ -208,6 +255,9 @@ func (c *Client) GetBill(id string) (*Bill, error) {
 	return &result, err
 }
 
+// DeleteBill deletes a bill with the given ID.
+// An error will be returned if the bill is not found, or
+// if the HTTP request fails.
 func (c *Client) DeleteBill(id string) error {
 	req, err := c.newRequest(http.MethodDelete, "/bills/"+id, nil)
 	if err != nil {
@@ -221,6 +271,11 @@ func (c *Client) DeleteBill(id string) error {
 	return err
 }
 
+// CheckRegistration checks for a bank account's registration status based on
+// the given account number. Returns true if the bank account is registered
+// and verified, and false otherwise.
+// An error will be returned if the bank account is not found or if the
+// HTTP request fails.
 func (c *Client) CheckRegistration(accountNumber string) (bool, error) {
 	req, err := c.newRequest(http.MethodGet, "/check/bank_account_number/"+accountNumber, nil)
 	if err != nil {
@@ -242,6 +297,14 @@ func (c *Client) CheckRegistration(accountNumber string) (bool, error) {
 	return false, ErrBankAccountNotFound
 }
 
+// GetBillTransactions retrieves a set of transactions related to a bill with the given ID.
+// Up to 15 transactions will be returned at a time.
+// The page parameter determines the page of the open collection set to retrieve,
+// and defaults to 1.
+// The status parameter determines whether to retrieve all transactions or
+// only transactions that are pending, completed or failed. This parameter can
+// take the values "", "pending", "completed", or "failed", and will default to "".
+// An error will be returned if the HTTP request fails.
 func (c *Client) GetBillTransactions(id string, page int, status string) (*BillTransactions, error) {
 	if page <= 0 {
 		page = 1
@@ -267,6 +330,9 @@ func (c *Client) GetBillTransactions(id string, page int, status string) (*BillT
 	return &result, err
 }
 
+// GetPaymentMethodIndex retrieves all available payment methods that can be
+// enabled or disabled on a collection with the given ID.
+// An error will be returned if the HTTP request fails.
 func (c *Client) GetPaymentMethodIndex(id string) (*[]PaymentMethod, error) {
 	req, err := c.newRequest(http.MethodGet, "/collections/"+id+"/payment_methods", nil)
 	if err != nil {
@@ -278,6 +344,10 @@ func (c *Client) GetPaymentMethodIndex(id string) (*[]PaymentMethod, error) {
 	return result.PaymentMethods, err
 }
 
+// UpdatePaymentMethods enables a set of payment methods on a collection with the
+// given ID.
+// The payment method codes are passed as a slice of strings.
+// An error will be returned if the HTTP request fails.
 func (c *Client) UpdatePaymentMethods(id string, codes []string) (*[]PaymentMethod, error) {
 	methods := []PaymentMethod{}
 	for _, element := range codes {
@@ -300,6 +370,10 @@ func (c *Client) UpdatePaymentMethods(id string, codes []string) (*[]PaymentMeth
 	return result.PaymentMethods, err
 }
 
+// GetBankAccountIndex gets a set of bank accounts with the given account numbers.
+// This function requires the Billplz 'ADMIN' setting to be turned on, and will return
+// an error if this condition is not met.
+// An error will also be returned if the HTTP request fails.
 func (c *Client) GetBankAccountIndex(accountNumbers []string) (*BankAccountList, error) {
 	req, err := c.newRequest(http.MethodGet, "/bank_verification_services", nil)
 	if err != nil {
@@ -323,6 +397,10 @@ func (c *Client) GetBankAccountIndex(accountNumbers []string) (*BankAccountList,
 	return &result, err
 }
 
+// GetBankAccount gets a bank account with the given account number.
+// This function requires the Billplz 'ADMIN' setting to be turned on, and will return
+// an error if this condition is not met.
+// An error will also be returned if the HTTP request fails.
 func (c *Client) GetBankAccount(accountNumber string) (*BankAccount, error) {
 	req, err := c.newRequest(http.MethodGet, "/bank_verification_services/"+accountNumber, nil)
 	if err != nil {
@@ -338,6 +416,12 @@ func (c *Client) GetBankAccount(accountNumber string) (*BankAccount, error) {
 	return &result, err
 }
 
+// CreateBankAccount creates a new bank account through the API's Bank Account
+// Direct Verification Service.
+// This function requires the Billplz 'ADMIN' setting to be turned on, and will return
+// an error if this condition is not met.
+// An error will also be returned if the supplied bank account fails validation,
+// or if the HTTP request fails.
 func (c *Client) CreateBankAccount(b BankAccount) (*BankAccount, error) {
 	err := b.Validate()
 	if err != nil {
